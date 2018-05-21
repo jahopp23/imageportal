817d6eee3cc60475b28d938691a0e05f1e9f1dfb
John A. Hopper <jahopp@gmail.com>
2018-05-20 22:27:23 -0500

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20161216083851) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "images", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
